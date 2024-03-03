67ab2a9fba9b843e2a4b237f2a5efe97a061d8da
develqhy <ikettle9000@163.com>
2024-03-03 13:03:57 +0800

    项目搭建-整合MyBatis Plus

--- yunshang-oa-api/src/main/java/com/yunshang/oa/ServiceOaAuthApplication.java
+++ yunshang-oa-api/src/main/java/com/yunshang/oa/ServiceOaAuthApplication.java
@@ -1,9 +1,11 @@
 package com.yunshang.oa;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.yunshang.oa.mapper")
 public class ServiceOaAuthApplication {
 
     public static void main(String[] args) {
